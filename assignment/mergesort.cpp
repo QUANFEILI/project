7dc30c2568b9eca25223c0e2cc8afe5a9413c469
“QUANFEI” <qli18@uncc.edu>
2025-02-05 20:15:28 -0500

    merge sort for assignment

---
+++ assignment/mergesort.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+
+void merge(vector<int> &arr, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    vector<int> L(n1), R(n2);
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int i = 0; i < n2; i++)
+        R[i] = arr[mid + 1 + i];
+
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j])
+            arr[k++] = L[i++];
+        else
+            arr[k++] = R[j++];
+    }
+
+    while (i < n1)
+        arr[k++] = L[i++];
+    while (j < n2)
+        arr[k++] = R[j++];
+}
+
+void mergeSort(vector<int> &arr, int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        cerr << "Usage: " << argv[0] << " <size_of_array>" << endl;
+        return 1;
+    }
+
+    int n = atoi(argv[1]);
+    vector<int> arr(n);
+
+    srand(time(nullptr));
+    for (int i = 0; i < n; i++)
+        arr[i] = rand() % 1000000;
+
+    auto start = high_resolution_clock::now();
+    mergeSort(arr, 0, n - 1);
+    auto stop = high_resolution_clock::now();
+
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cout << "Array size: " << n << ", Time: " << duration.count() << " ms" << endl;
+
+    return 0;
+}
